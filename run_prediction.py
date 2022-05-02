6395e5fe1efd1e88679a393e4d2912817d8882ed
csanny <47472862+csanny@users.noreply.github.com>
2022-05-02 13:52:29 +0900

    Update run_prediction.py

--- run_prediction.py
+++ run_prediction.py
@@ -1,33 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+# import module
 import torch
 from NERDA.models import NERDA
 
-
-# In[2]:
-
-
+# import model
 model = torch.load('SurviceBERT_V2_32-16.pt')
 
-
-# In[3]:
-
-
+# predict NER ex1
 model.predict_text('During the period 12 January to 15 March 2022, a total of 53 suspected yellow fever cases, including six deaths, have been reported from Isiolo county, central Kenya.')
 
-
-# In[4]:
-
-
+# predict NER ex2
 model.predict_text('On 5 April 2022, WHO was notified of 10 cases of severe acute hepatitis of unknown aetiology in children under the age of 10 years, across central Scotland.')
-
-
-# In[ ]:
-
-
-
-
